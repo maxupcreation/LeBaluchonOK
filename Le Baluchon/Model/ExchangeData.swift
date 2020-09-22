272c1b73481b5da293c83e47d967e4c1d5146c53
Maxime <maxupcreation@gmail.com>
2020-09-22 17:42:14 +0200

    Network call - Exchange (0.1)

---
+++ Le Baluchon/Model/ExchangeData.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Le Baluchon
+//
+//  Created by Maxime on 21/09/2020.
+//  Copyright © 2020 Maxime. All rights reserved.
+//
+
+import Foundation
+struct ExchangeData {
+    var euroValue : String?
+    var dollarValue : String?
+    var rates : Dictionary <String, Float>
+}
--- Le Baluchon/Model/ExchangeServiceModel.swift
+++ Le Baluchon/Model/ExchangeServiceModel.swift
@@ -9,21 +9,55 @@
 import Foundation
 class  ExchangeServiceModel {
     
-       private func createConversionRequest() -> URLRequest? {
     
-           guard let conversionUrl = URL(string: "http://data.fixer.io/api/latest?access_key=79d9d449523d341c9a5dd2d8b3328419") else {
-               return nil
-           }
-           var request = URLRequest(url: conversionUrl )
-           request.httpMethod = "POST"
-           
-           /* let body = "method=getQuote&lang=en&format=json"
-           request.httpBody = body.data(using: .utf8) */
-           
-           return request
-       }
+    private var task : URLSessionTask?
+    let session = URLSession(configuration: .default)
     
     
+    enum NetworkError  : Error {
+        case noData
+        case noResponse
+        case undecodable
+    }
+    
+    
+    func getConversionRequestUrl() -> URLRequest? {
+        
+        guard let conversionUrl = URL(string: "http://data.fixer.io/api/latest?access_key=79d9d449523d341c9a5dd2d8b3328419") else {
+            return nil
+        }
+        var request = URLRequest(url: conversionUrl )
+        request.httpMethod = "POST"
+        
+        
+        /* let body = "method=getQuote&lang=en&format=json"
+         request.httpBody = body.data(using: .utf8) */
+        
+        return request
+    }
+    
+    func createConversionRequestTask(callback : @escaping (Result<ExchangeData,NetworkError>)->Void) {
+        
+        let request = getConversionRequestUrl()
+        
+        task = session.dataTask(with: request! ) {
+            (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                callback(.failure(.noData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                callback(.failure(.noResponse))
+                return
+            }
+            
+        }
+        
+        task?.resume()
+        
+    }
     
     
 }
